bd42d46e81c2cf467960df5e9c3a6d5c2401a754
Odnel Trumbore <otrumbore@gmail.com>
2024-04-13 13:32:06 -0400

    added socials drawer for mobile and smller screens

---
+++ components/socialsDrawer.jsx
@@ -0,0 +1,71 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { IoShareSocialSharp } from 'react-icons/io5';
+import { Button } from './ui/button';
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from '@/components/ui/drawer';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { MdAlternateEmail } from 'react-icons/md';
+
+const SocialsDrawer = () => {
+	const [socialDrawerBtn, setSocialDrawerBtn] = useState(false);
+	const toggleSocialDrawerBtn = () => {
+		setSocialDrawerBtn(!socialDrawerBtn);
+	};
+	useEffect(() => {
+		setSocialDrawerBtn(true);
+	}, []);
+	return (
+		<div className='lg:hidden z-[999] fixed bottom-2 right-2'>
+			<div
+				className={`${
+					socialDrawerBtn ? 'block' : 'hidden'
+				} w-xl h-xl bg-gray-800 text-white px-3 pt-2 pb-1 rounded-md bg-opacity-80`}
+			>
+				<Drawer onClose={toggleSocialDrawerBtn}>
+					<DrawerTrigger>
+						<IoShareSocialSharp onClick={toggleSocialDrawerBtn} size={30} />
+					</DrawerTrigger>
+					<DrawerContent className='bg-gray-800 text-white'>
+						<DrawerHeader>
+							<DrawerTitle className='text-center'>
+								Catch me off my portfolio, how bout dat?
+							</DrawerTitle>
+							<DrawerDescription className='mt-4'>
+								<ul className='flex justify-center items-center gap-14 text-white'>
+									<li>
+										<FaLinkedinIn size={30} />
+									</li>
+									<li>
+										<FaGithub size={30} />
+									</li>
+									<li>
+										<MdAlternateEmail size={30} />
+									</li>
+								</ul>
+							</DrawerDescription>
+						</DrawerHeader>
+						<DrawerFooter>
+							<DrawerClose>
+								<Button variant='default' className='w-full max-w-md'>
+									Close
+								</Button>
+							</DrawerClose>
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
+			</div>
+		</div>
+	);
+};
+
+export default SocialsDrawer;
