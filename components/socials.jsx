707d3302fe546877d57727f7323ca79eaf52831d
Odnel Trumbore <otrumbore@gmail.com>
2024-04-13 11:24:15 -0400

    added tooltip to social links

--- components/socials.jsx
+++ components/socials.jsx
@@ -4,6 +4,12 @@ import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { MdAlternateEmail } from 'react-icons/md';
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 const Socials = () => {
 	const [isVisible, setIsVisible] = useState(false);
@@ -19,17 +25,49 @@ const Socials = () => {
 				animate={{ x: isVisible ? 0 : -100 }} // Final position (slide in from the left)
 				transition={{ type: 'spring', damping: 30, stiffness: 100 }}
 			>
-				<ul className='flex flex-col justify-center items-center space-y-1 pt-4'>
-					<li className='border-2 border-transparent hover:border-gray-300 p-[.50rem] rounded-md hover:shadow-lg shawdow-gray-200 cursor-pointer'>
-						<FaLinkedinIn size={30} />
-					</li>
-					<li className='border-2 border-transparent hover:border-gray-300 p-[.50rem] rounded-md hover:shadow-lg shawdow-gray-200 cursor-pointer'>
-						<FaGithub size={30} />
-					</li>
-					<li className='border-2 border-transparent hover:border-gray-300 p-[.50rem] rounded-md hover:shadow-lg shawdow-gray-200 cursor-pointer'>
-						<MdAlternateEmail size={30} />
-					</li>
-				</ul>
+				<TooltipProvider>
+					<ul className='flex flex-col justify-center items-center space-y-1 pt-4'>
+						<Tooltip>
+							<TooltipTrigger>
+								<li className='border-2 border-transparent hover:border-gray-300 p-[.50rem] rounded-md hover:shadow-lg shawdow-gray-200 cursor-pointer'>
+									<a
+										href='https://www.linkedin.com/in/odnel-trumbore'
+										target='_blank'
+									>
+										<FaLinkedinIn size={30} />
+									</a>
+								</li>
+								<TooltipContent side='right'>
+									<p>LinkedIn</p>
+								</TooltipContent>
+							</TooltipTrigger>
+						</Tooltip>
+						<Tooltip>
+							<TooltipTrigger>
+								<li className='border-2 border-transparent hover:border-gray-300 p-[.50rem] rounded-md hover:shadow-lg shawdow-gray-200 cursor-pointer'>
+									<a href='https://www.github.com/otrumbore' target='_blank'>
+										<FaGithub size={30} />
+									</a>
+								</li>
+							</TooltipTrigger>
+							<TooltipContent side='right'>
+								<p>GitHub</p>
+							</TooltipContent>
+						</Tooltip>
+						<Tooltip>
+							<TooltipTrigger>
+								<li className='border-2 border-transparent hover:border-gray-300 p-[.50rem] rounded-md hover:shadow-lg shawdow-gray-200 cursor-pointer'>
+									<a href='mailto:otrumbore@gmail.com' target='_blank'>
+										<MdAlternateEmail size={30} />
+									</a>
+								</li>
+							</TooltipTrigger>
+							<TooltipContent side='right'>
+								<p>Email Me!</p>
+							</TooltipContent>
+						</Tooltip>
+					</ul>
+				</TooltipProvider>
 			</motion.div>
 		</div>
 	);
