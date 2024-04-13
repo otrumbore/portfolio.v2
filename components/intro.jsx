4fac1000684fb9570f16969cd2dcf786ee79d7db
Odnel Trumbore <otrumbore@gmail.com>
2024-04-13 13:39:51 -0400

    updated some classes for responvie design

--- components/intro.jsx
+++ components/intro.jsx
@@ -18,16 +18,16 @@ const Intro = () => {
 			initial={{ opacity: 0 }}
 			animate={{ opacity: isVisible ? 1 : 0 }}
 			transition={{ duration: 0.75 }}
-			className='flex flex-col justify-center items-center h-screen'
+			className='flex flex-col justify-center items-center h-screen max-lg:landscape:h-[150vh]'
 		>
 			<h1
 				className={`${introFont.className} text-center text-6xl font-extrabold mb-16`}
 			>
 				<span className='sm:hidden'>Hey, I'm Odnel</span>
-				<span className='hidden sm:block'>Hey, I'm Odnel Trumbore</span>
+				<span className='hidden sm:block'>Hi, I'm Odnel Trumbore</span>
 			</h1>
 
-			<p className='text-2xl px-4 w-full text-center sm:w-1/2 mb-16'>
+			<p className='text-2xl px-4 w-full text-center lg:w-1/2 mb-16'>
 				As a dedicated Web Developer, I specialize in crafting and optimizing
 				dynamic websites and web applications, driven by a passion for
 				delivering innovative digital solutions that elevate user experiences
