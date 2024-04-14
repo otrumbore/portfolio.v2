efb2be447b31a5fa7f3dba6cc659347398f54a84
Odnel Trumbore <otrumbore@gmail.com>
2024-04-14 12:56:23 -0400

    added more  UI componenets

---
+++ components/ui/textarea.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const Textarea = React.forwardRef(({ className, ...props }, ref) => {
+	return (
+		<textarea
+			className={cn(
+				'flex min-h-[240px] w-full rounded-md border-4 border-input bg-transparent px-3 py-3 text-md shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+				className
+			)}
+			ref={ref}
+			{...props}
+		/>
+	);
+});
+Textarea.displayName = 'Textarea';
+
+export { Textarea };
