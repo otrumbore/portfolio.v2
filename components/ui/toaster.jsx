2eafd9f590ebd06b62ebe04e1d6f5b7e429b3eef
Odnel Trumbore <otrumbore@gmail.com>
2024-04-15 13:14:04 -0400

    changed toast ui components to allow for a type of message and show icon if certain type

--- components/ui/toaster.jsx
+++ components/ui/toaster.jsx
@@ -1,35 +1,47 @@
-"use client"
+'use client';
 
 import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+	Toast,
+	ToastClose,
+	ToastDescription,
+	ToastProvider,
+	ToastTitle,
+	ToastViewport,
+} from '@/components/ui/toast';
+import { useToast } from '@/components/ui/use-toast';
+import { ImCheckmark } from 'react-icons/im';
 
 export function Toaster() {
-  const { toasts } = useToast()
+	const { toasts } = useToast();
 
-  return (
-    (<ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          (<Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>)
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>)
-  );
+	return (
+		<ToastProvider>
+			{toasts.map(function ({
+				id,
+				title,
+				description,
+				action,
+				type,
+				...props
+			}) {
+				return (
+					<Toast key={id} {...props}>
+						<div className='grid gap-1'>
+							<div className='flex gap-4'>
+								{type && type === 'success' ? <ImCheckmark size={15} /> : ''}
+								{title && <ToastTitle>{title}</ToastTitle>}
+							</div>
+
+							{description && (
+								<ToastDescription>{description}</ToastDescription>
+							)}
+						</div>
+						{action}
+						<ToastClose />
+					</Toast>
+				);
+			})}
+			<ToastViewport />
+		</ToastProvider>
+	);
 }
