3d31bc9db56b310a3e1776e0f5d013177eec9200
Odnel Trumbore <otrumbore@gmail.com>
2024-05-06 15:49:15 -0400

    added upcoming project, changed from flex to grid for projects

--- components/projects.jsx
+++ components/projects.jsx
@@ -10,6 +10,7 @@ import Image from 'next/image';
 import LCCImg from '@/public/project_imgs/Project_LCC_W_Mobile.png';
 import PortfolioV1Img from '@/public/project_imgs/project_port_v1.png';
 import PortfolioV2Img from '@/public/project_imgs/project_port_v2.png';
+import BoxSubImg from '@/public/project_imgs/Project_BoxSub.png';
 
 const headingFont = Oswald({ subsets: ['latin'], weight: ['600'] });
 
@@ -44,26 +45,28 @@ const Projects = () => {
 							<p className='text-center mb-8 text-2xl font-semibold'>
 								Professional Projects
 							</p>
-							<div className='w-full flex flex-col items-center lg:flex-row lg:justify-between gap-6'>
+							<div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-8'>
 								<div className='flex flex-col'>
 									<h4 className='text-center text-xl font-semibold'>
 										<a href='https://louisascraftycorner.com' target='_blank'>
 											Louisa&apos;s Crafty Corner
 										</a>
 									</h4>
-									<div className='flex flex-col lg:flex-col'>
-										<a
-											href='https://louisascraftycorner.com'
-											target='_blank'
-											className='hover:scale-110'
-										>
-											<Image
-												src={LCCImg}
-												width={500}
-												height={500}
-												alt='Picture of the author'
-											/>
-										</a>
+									<div className='flex flex-col'>
+										<div className='flex justify-center'>
+											<a
+												href='https://louisascraftycorner.com'
+												target='_blank'
+												className='hover:scale-110'
+											>
+												<Image
+													src={LCCImg}
+													width={500}
+													height={500}
+													alt='Picture of the author'
+												/>
+											</a>
+										</div>
 
 										<div className='mt-4 flex flex-col gap-4 w-full items-center justify-center'>
 											<p>E-Commerce Store</p>
@@ -75,24 +78,51 @@ const Projects = () => {
 										</div>
 									</div>
 								</div>
+								<div className='flex flex-col'>
+									<h4 className='text-center text-xl font-semibold'>
+										Upcoming Project
+									</h4>
+									<div className='flex flex-col'>
+										<div className='w-full flex justify-center hover:scale-110'>
+											<Image
+												src={BoxSubImg}
+												width={500}
+												height={500}
+												alt='BoxSub Image'
+											/>
+										</div>
+
+										<div className='mt-4 flex flex-col gap-4 w-full items-center justify-center'>
+											<p>Marketplace</p>
+											<p className='text-center'>
+												<span className='font-semibold'>Tech Stack: </span>
+												MERN Stack: MongoDB, Express.js, Next.js, Node.js with
+												Tailwind, Framer Motion, Radix, Shadcn
+											</p>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 						<div className='w-full p-4 xl:w-2/3'>
 							<p className='text-center mb-8 text-2xl font-semibold'>
 								Personal Projects
 							</p>
-							<div className='w-full flex flex-col items-center lg:flex-row lg:justify-between gap-6'>
+							<div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-8'>
 								<div className='flex flex-col'>
 									<h4 className='text-center text-xl font-semibold'>
 										Portfolio V2
 									</h4>
-									<div className='flex flex-col lg:flex-col'>
-										<Image
-											src={PortfolioV2Img}
-											width={500}
-											height={500}
-											alt='Picture of the author'
-										/>
+									<div className='flex flex-col'>
+										<div className='flex justify-center'>
+											<Image
+												src={PortfolioV2Img}
+												width={500}
+												height={500}
+												alt='Picture of the author'
+											/>
+										</div>
+
 										<div className='mt-4 flex flex-col gap-4 w-full items-center justify-center'>
 											<p>New portfolio you are currently viewing</p>
 											<p className='text-center'>
@@ -109,15 +139,20 @@ const Projects = () => {
 										</a>
 									</h4>
 									<div className='flex flex-col lg:flex-col'>
-										<a href='http://odneltrumbore.netlify.app' target='_blank'>
-											<Image
-												src={PortfolioV1Img}
-												width={500}
-												height={500}
-												alt='Picture of the author'
-												className='hover:scale-110'
-											/>
-										</a>
+										<div className='flex justify-center'>
+											<a
+												href='http://odneltrumbore.netlify.app'
+												target='_blank'
+											>
+												<Image
+													src={PortfolioV1Img}
+													width={500}
+													height={500}
+													alt='Picture of the author'
+													className='hover:scale-110'
+												/>
+											</a>
+										</div>
 										<div className='mt-4 flex flex-col gap-4 w-full items-center justify-center'>
 											<p>My original and first portfolio.</p>
 											<p className='text-center'>
